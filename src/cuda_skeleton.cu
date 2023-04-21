ed03b25d0afc42291eab227ddfc33ada5a72eaad
Zhijian Mei <545902915@qq.com>
2023-04-21 23:32:51 +0800

    1

--- src/cuda_skeleton.cu
+++ src/cuda_skeleton.cu
@@ -5,11 +5,7 @@ using namespace std;
 __global__ void test_Kernel(int* C,int* num_v1,int* num_v2)
 {
     int threadID = threadIdx.x;
-    if (threadID == 0){
-        C[threadID] = *num_v1;
-    } else{
-    C[threadID] = *num_v2;
-    }
+    
 }
 
 void cuda_query(string dir, int num_blocks_per_grid, int num_threads_per_block, int* queryAns) {
@@ -25,7 +21,8 @@ void cuda_query(string dir, int num_blocks_per_grid, int num_threads_per_block, 
     size_t size_num_v2 = sizeof(int);
     size_t size_h_lrval_index_u = sizeof(lrval_index_block*)*h_lrval_index_u[0].size()*h_lrval_index_u.size();
     size_t size_h_lrval_index_v = sizeof(lrval_index_block*)*h_lrval_index_v[0].size()*h_lrval_index_v.size();
-    
+    cout<<size_h_lrval_index_u<<"\n";
+    exit(0);
     // int *h_c,*d_c;
     // h_c = (int*)malloc(size);
     // cudaMalloc((void**)&d_c,size);
@@ -45,14 +42,13 @@ void cuda_query(string dir, int num_blocks_per_grid, int num_threads_per_block, 
     cudaMemcpy(d_num_v1,&h_g.num_v1,size_num_v1,cudaMemcpyHostToDevice);
     cudaMemcpy(d_num_v2,&h_g.num_v2,size_num_v2,cudaMemcpyHostToDevice);
 
-    int *h_c,*d_c;
-    h_c = (int*)malloc(size);
-    cudaMalloc((void**)&d_c,size);
-    cudaMemcpy(d_c,h_c,size,cudaMemcpyHostToDevice);
-    test_Kernel<<<num_blocks_per_grid,num_threads_per_block>>>(d_c,d_num_v1,d_num_v2);
-    cudaMemcpy(h_c,d_c,size,cudaMemcpyDeviceToHost);
-    cout<<h_c[0]<<" "<<h_c[1]<<"\n";
-    exit(0);
+    // int *h_c,*d_c;
+    // h_c = (int*)malloc(size);
+    // cudaMalloc((void**)&d_c,size);
+    // cudaMemcpy(d_c,h_c,size,cudaMemcpyHostToDevice);
+    // test_Kernel<<<num_blocks_per_grid,num_threads_per_block>>>(d_c,d_num_v1,d_num_v2);
+    // cudaMemcpy(h_c,d_c,size,cudaMemcpyDeviceToHost);
+    // exit(0);
 
     vector<vector<lrval_index_block*>> *d_lrval_index_u;
     vector<vector<lrval_index_block*>> *d_lrval_index_v;
--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,4 @@
-
-cd /project/msbd5009_stu01/ass3/src
-
 nvcc -std=c++11 cuda_skeleton.cu decom.cpp main.cpp -o lrds
 
-./lrds -ParallelQuery ../data/WR/ 4 128
+./lrds -ParallelQuery ../data/example/ 1 2
 
