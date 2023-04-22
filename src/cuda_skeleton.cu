31c11547995a242c079a0717515afefac519a2da
Zhijian Mei <545902915@qq.com>
2023-04-22 10:40:38 +0800

    Update cuda_skeleton.cu

--- src/cuda_skeleton.cu
+++ src/cuda_skeleton.cu
@@ -13,8 +13,6 @@ void cuda_query(string dir, int num_blocks_per_grid, int num_threads_per_block, 
 	lrIndexBasic(h_g);    
     vector<vector<lrval_index_block*>> h_lrval_index_u; vector<vector<lrval_index_block*>> h_lrval_index_v;
     build_lrval_index(h_g, h_lrval_index_u, h_lrval_index_v);
-    int num_h_lrval_index_u = 0;
-    int num_h_lrval_index_v = 0;
 
     size_t size = 2 * sizeof(int);
     size_t size_num_v1 = sizeof(int);
